e2cc2785a8d5bee923e00a071d2adea1fc220958
Vladyslav Andrievych <vladandrievych@gmail.com>
2023-12-04 21:07:37 +0200

    added header markup and styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,9 @@
 import type { AppProps } from 'next/app';
 
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
+
 import '@/src/styles/globals.scss';
 
 export default function App({ Component, pageProps }: AppProps) {
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,8 +1,12 @@
 import Head from 'next/head';
+import Image from 'next/image';
 
 import styles from './layout.module.scss';
 
 import { robotoRegular } from '@/src/fonts/fonts';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 
 export default function Layout({
   children,
@@ -17,7 +21,19 @@ export default function Layout({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <div className={`root ${robotoRegular.className}`}>
-        <header className={styles.header}></header>
+        <header className={styles.header}>
+          <Image
+            src="/images/logo.png"
+            height={45}
+            width={45}
+            alt="logo"
+            className={styles.headerLogo}
+          />
+          <FontAwesomeIcon
+            icon={faPenToSquare}
+            className={styles.headerEditIcon}
+          />
+        </header>
         <main>{children}</main>
         <footer className={styles.footer}></footer>
       </div>
